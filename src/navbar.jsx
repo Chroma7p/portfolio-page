6b1e21f5e87e857ed8b4a8fdfbc0ed59121ab4ae
chroma7p <nahco3.hcl@gmail.com>
2022-06-02 20:39:21 +0900

    レスポンシブ完成

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import MediaQuery from 'react-responsive';
+
 const homeurl=process.env.PUBLIC_URL;
 export const Navbar = () => {
     return (
@@ -9,7 +9,7 @@ export const Navbar = () => {
                 <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <span className="text-slate-100 md:ml-3 md:text-xl text-3xl">Portfolio</span>
                 </a>
-                <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                <nav className="md:ml-auto flex flex-wrap items-center text-lg md:text-base justify-center">
                     <Link to={homeurl+"/"} className="mx-5 hover:text-gray-300">Top</Link>
                     <Link to={homeurl+"/work"} className="mx-5 hover:text-gray-300">Work</Link>
                     <Link to={homeurl+"/hobby"} className="mx-5 hover:text-gray-300">Hobby</Link>
